e1681fad7c6035ccb68e562def8c213d615f6883
redGlopy <tamirarbel10@gmail.com>
2026-04-24 15:39:15 +0300

    added helpful comments and some minor changes

--- main.cpp
+++ main.cpp
@@ -2,25 +2,34 @@
 #include <iostream>
 
 int main() {
+
+    // takes iformation from the spesified webcam
     cv::VideoCapture cap(0);
 
+    // checks if the webcam is open
     if (!cap.isOpened()) {
         std::cerr << "error: could not open camera" << std::endl;
         return -1;
     }
 
+    // a frame to display later
     cv::Mat frame;
 
-    std::string windowName = "webcam feed";
-    cv::namedWindow(windowName);
+    // creates a window to display the frame on
+    cv::namedWindow("webcam feed");
 
-    while (cv::getWindowProperty(windowName, cv::WND_PROP_VISIBLE) >= 1) {
+    // the main loop that displays the frame
+    while (cv::getWindowProperty("webcam feed", cv::WND_PROP_VISIBLE) >= 1) {
+
+        //puts the information fromt he start into the frame
         cap >> frame;
         if (frame.empty()) break;
 
-        cv::imshow(windowName, frame);
+        // updates the window to display the frame
+        cv::imshow("webcam feed", frame);
 
-        if (cv::waitKey(1) == 27) break; 
+        // close the window and brake the loop if Esc is pressed
+        if (cv::waitKey(10) == 27) break; 
     }
 
     return 0;
